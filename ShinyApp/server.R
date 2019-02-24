6b55263548b8709c5de467b12dae49db892597b2
David Ossa Saldarriaga <36513198+dossas95@users.noreply.github.com>
2019-02-24 11:27:15 -0500

    Shiny App

--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -6,37 +6,18 @@ library(lubridate)
 
 
 shinyServer(function(input, output) {
-  
   observeEvent(input$apply2, {
     showModal(modalDialog(
-      title = "Resultados de la predicción",
+      title = "Resultados del analisis de imagen",
       footer = modalButton("Cerrar"),
       easyClose = TRUE,
       fluidRow(column(10, align = "center", offset = 1,
                       output$tabla<-renderTable(
                         if(input$apply2){
-                          daygen(input$fecha_inicial,input$fecha_final)
+                          daygen(input$picture)
                         }
                       )
       ))
-      
-    ))
-  })
-  
-  
-  observeEvent(input$apply3, {
-    showModal(modalDialog(
-      title = "Resumen descriptivo de la predicción",
-      footer = modalButton("Cerrar"),
-      easyClose = TRUE,
-      
-      output$grafico<-renderPlot(
-        if(input$apply3){
-          daydescri(input$fecha_inicial,input$fecha_final)
-        }
-      )
-      
-      
     ))
   })
 })
--- ShinyApp/ui.R
+++ ShinyApp/ui.R
@@ -5,12 +5,17 @@ library(Hmisc)
 library(lubridate)
 library(shinythemes)
 shinyUI(fluidPage(
-  theme = "style.css",
-  absolutePanel(class = "wrapper", 
-                fluidRow(column(10, align = "center", offset = 1,  
-                                h2("Sunglasses Detector")
-                )),
-                fluidRow(column(12, align = "center", fileInput("picture", "Choose a file (PNG or JPEG)", 
-                                   accept = c('image/png', 'image/jpeg'), width = '60%'))
-  )
+theme = "style.css",
+fillRow(navbarPage("Detector de lentes",
+                   tabPanel(class="wrapper", "Aplicacion",
+                            fluidRow(column(10, align = "center", offset = 1, h2("Detector de lentes"))),
+                            fluidRow(column(12, align = "center", fileInput("picture", "Selecciona un archivo(PNG/JPEG)",
+                                                                            accept = c('image/png', 'image/jpeg'),
+                                                                            width = '60%'))),
+                            fluidRow(column(12, align = "center", actionButton("apply", label = "Analizar imagen")))),
+                   
+                   tabPanel(class="wrapper", "Explicacion",
+                            fluidRow(column(10, align = "center", offset = 1,
+                           h2("Funcionamiento del modelo"))))
+                  )
 )))
