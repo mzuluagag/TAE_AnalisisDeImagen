d8bd17414d7f40f8f3baf960554b83a760c53768
mzuluagag <mateo.zu.gi1999@gmail.com>
2019-02-25 23:19:35 -0500

    sd

--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -11,10 +11,15 @@ shinyServer(function(input, output) {
       title = "Resultados del analisis de imagen",
       footer = modalButton("Cerrar"),
       easyClose = TRUE,
-      fluidRow(column(10, align = "center", offset = 1,
+      fluidRow(column(10, align = "center", offset = 1,... = 2,
                       output$result <- renderText(
                         if(input$apply){
-                          DetectorGafas(input$picture)
+                          if(DetectorGafas(input$picture) == 1){
+                            "Tiene gafas"
+                          }
+                          else{
+                            "No tiene gafas"
+                          }
                         }
                       ),
                       output$result <- renderPlot(
